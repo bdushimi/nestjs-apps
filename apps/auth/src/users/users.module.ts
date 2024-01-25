bab5497699645192c1feed94b71646b3be935b78
bdushimi <lespharaons@hotmail.com>
2024-01-26 00:51:09 +0200

    Refactors the loading of Configurations & adds auth module boilerplate codes

---
+++ apps/auth/src/users/users.module.ts
@@ -0,0 +1,32 @@
+import { Module } from '@nestjs/common';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { DatabaseModule, LoggerModule } from '@app/common';
+import { UserDocument, UserSchema } from './models/user.schema';
+import { UsersRepository } from './users.repository';
+import * as Joi from 'joi';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    DatabaseModule,
+    DatabaseModule.forFeature([
+      {
+        name: UserDocument.name,
+        schema: UserSchema,
+      },
+    ]),
+    LoggerModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required(),
+        JWT_SECRET: Joi.string().required(),
+        JWT_EXPIRATION: Joi.string().required(),
+      }),
+    }),
+  ],
+  controllers: [UsersController],
+  providers: [UsersService, UsersRepository],
+})
+export class UsersModule {}
