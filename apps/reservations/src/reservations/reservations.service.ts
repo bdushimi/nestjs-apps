230e201609da22736c1feb7273ff259c7d1cc8d8
bdushimi <lespharaons@hotmail.com>
2024-02-01 01:37:45 +0200

    Configures the payment service to charge the test card on Stripe

--- apps/reservations/src/reservations/reservations.service.ts
+++ apps/reservations/src/reservations/reservations.service.ts
@@ -1,22 +1,30 @@
+import { PAYMENTS_SERVICE } from '@app/common';
 import { Inject, Injectable } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
 import { CreateReservationDto } from '../dto/create-reservation.dto';
 import { UpdateReservationDto } from '../dto/update-reservation.dto';
 import { ReservationRepository } from './reservations.respository';
-import { PAYMENTS_SERVICE } from '@app/common';
-import { ClientProxy } from '@nestjs/microservices';
+import { map } from 'rxjs';
 
 @Injectable()
 export class ReservationsService {
   constructor(
     private readonly reservationRepository: ReservationRepository,
-    @Inject(PAYMENTS_SERVICE) paymentsService: ClientProxy,
+    @Inject(PAYMENTS_SERVICE) private readonly paymentsService: ClientProxy,
   ) {}
   async create(createReservationDto: CreateReservationDto, userId: string) {
-    return this.reservationRepository.create({
-      ...createReservationDto,
-      timestamp: new Date(),
-      userId,
-    });
+    return this.paymentsService
+      .send('create_charge', createReservationDto.charge)
+      .pipe(
+        map((res) => {
+          return this.reservationRepository.create({
+            ...createReservationDto,
+            invoiceId: res.id,
+            timestamp: new Date(),
+            userId,
+          });
+        }),
+      );
   }
 
   async findAll() {
