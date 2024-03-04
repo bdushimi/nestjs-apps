4ce6a78d1c6f50a1409700a2385a17855b9f167d
bdushimi <lespharaons@hotmail.com>
2024-03-04 20:56:51 +0200

    Adds notification apps & its basic ops

--- apps/reservations/src/reservations/reservations.service.ts
+++ apps/reservations/src/reservations/reservations.service.ts
@@ -1,4 +1,4 @@
-import { PAYMENTS_SERVICE } from '@app/common';
+import { PAYMENTS_SERVICE, UserDto } from '@app/common';
 import { Inject, Injectable } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { CreateReservationDto } from '../dto/create-reservation.dto';
@@ -12,9 +12,15 @@ export class ReservationsService {
     private readonly reservationRepository: ReservationRepository,
     @Inject(PAYMENTS_SERVICE) private readonly paymentsService: ClientProxy,
   ) {}
-  async create(createReservationDto: CreateReservationDto, userId: string) {
+  async create(
+    createReservationDto: CreateReservationDto,
+    { email, _id: userId }: UserDto,
+  ) {
     return this.paymentsService
-      .send('create_charge', createReservationDto.charge)
+      .send('create_charge', {
+        ...createReservationDto.charge,
+        email,
+      })
       .pipe(
         map((res) => {
           return this.reservationRepository.create({
