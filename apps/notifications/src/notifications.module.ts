7e73f45e6224ab590e51e6ba2afa144f93c6fa73
bdushimi <lespharaons@hotmail.com>
2024-03-04 21:48:36 +0200

    Updates notification service to send email notifications

--- apps/notifications/src/notifications.module.ts
+++ apps/notifications/src/notifications.module.ts
@@ -1,9 +1,9 @@
+import { LoggerModule } from '@app/common';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 import { NotificationsController } from './notifications.controller';
 import { NotificationsService } from './notifications.service';
-import * as Joi from 'joi';
-import { ConfigModule } from '@nestjs/config';
-import { LoggerModule } from '@app/common';
 
 @Module({
   imports: [
@@ -11,6 +11,10 @@ import { LoggerModule } from '@app/common';
       isGlobal: true,
       validationSchema: Joi.object({
         PORT: Joi.number().required(),
+        SMTP_USER: Joi.string().required(),
+        SMTP_OAUTH_CLIENT_ID: Joi.string().required(),
+        GOOGLE_OAUTH_CLIENT_SECRET: Joi.string().required(),
+        GOOGLE_OAUTH_REFRESH_TOKEN: Joi.string().required(),
       }),
     }),
     LoggerModule,
--- apps/payments/src/payments.service.ts
+++ apps/payments/src/payments.service.ts
@@ -36,7 +36,10 @@ export class PaymentsService {
       currency: 'usd',
     });
 
-    this.notificationsService.emit('notify_email', { email });
+    this.notificationsService.emit('notify_email', {
+      email,
+      text: `Your payment of $${amount} has been completed successfully`,
+    });
 
     return paymentIntent;
   }
