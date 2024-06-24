3b53848086425d885d5130c98598d419de94c569
bdushimi <lespharaons@hotmail.com>
2024-06-24 16:09:36 +0200

    Adds Roles support & guards

--- libs/common/src/auth/jwt-auth.guard.ts
+++ libs/common/src/auth/jwt-auth.guard.ts
@@ -3,15 +3,23 @@ import {
   ExecutionContext,
   Inject,
   Injectable,
+  Logger,
+  UnauthorizedException,
 } from '@nestjs/common';
 import { Observable, catchError, map, of, tap } from 'rxjs';
 import { AUTH_SERVICE } from '../constants/services';
 import { ClientProxy } from '@nestjs/microservices';
 import { UserDto } from '../dto';
+import { Reflector } from '@nestjs/core';
 
 @Injectable()
 export class JwtAuthGuard implements CanActivate {
-  constructor(@Inject(AUTH_SERVICE) private readonly authClient: ClientProxy) {}
+  private readonly logger = new Logger(JwtAuthGuard.name);
+
+  constructor(
+    @Inject(AUTH_SERVICE) private readonly authClient: ClientProxy,
+    private readonly reflector: Reflector,
+  ) {}
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
@@ -20,17 +28,32 @@ export class JwtAuthGuard implements CanActivate {
       context.switchToHttp().getRequest().headers?.authentication;
     if (!jwt) return false;
 
+    const roles = this.reflector.get<string[]>('roles', context.getHandler());
+
     return this.authClient
       .send<UserDto>('authenticate', {
         Authentication: jwt,
       })
       .pipe(
         tap((res) => {
+          if (roles) {
+            for (const role of roles) {
+              if (!res.roles?.includes(role)) {
+                this.logger.log(
+                  `User ${res.email} does not have role valid roles`,
+                );
+                throw new UnauthorizedException();
+              }
+            }
+          }
           //The response, if the user is authenticated, will be just the user associated with the token passed to the auth service.
           context.switchToHttp().getRequest().user = res;
         }),
         map(() => true),
-        catchError(() => of(false)),
+        catchError((error) => {
+          this.logger.error(error);
+          return of(false);
+        }),
       );
   }
 }
